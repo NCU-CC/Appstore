78b0080634d4cc970d9f3d6da6dba0f94becd972
JackSie <jetswayss@gmail.com>
2015-02-02 16:12:50 +0800

    add file caching

--- src/main/java/tw/edu/ncu/cc/appstore/controller/UploadFileController.java
+++ src/main/java/tw/edu/ncu/cc/appstore/controller/UploadFileController.java
@@ -5,6 +5,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.ServletActionContext;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -27,8 +30,19 @@ public class UploadFileController extends ActionSupport{
         File fileToDownload = new File(path); 
         inputStream = new FileInputStream(fileToDownload);
         fileName = fileToDownload.getName();
-        contentLength = fileToDownload.length();         
+        contentLength = fileToDownload.length();
+        setCaching();
         return SUCCESS;
+    }
+    
+    private void setCaching(){
+        HttpServletResponse res =ServletActionContext.getResponse();
+        final int CACHE_DURATION_IN_SECOND = 60 * 60 * 24 * 2; // 2 days
+        final long   CACHE_DURATION_IN_MS = CACHE_DURATION_IN_SECOND  * 1000;
+        long now = System.currentTimeMillis();
+        ((HttpServletResponse) res).addHeader("Cache-Control", "max-age=" + CACHE_DURATION_IN_SECOND);
+        ((HttpServletResponse) res).addHeader("Cache-Control", "must-revalidate");//optional
+        ((HttpServletResponse) res).setDateHeader("Expires", now + CACHE_DURATION_IN_MS);
     }
     
     public InputStream getInputStream() {
